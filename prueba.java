c8cc282d37cbdea05e26cc45f09109d0feb0db3f
Patryk333 <patkow@alu.edu.gva.es>
2025-03-07 10:56:38 +0100

    8

--- prueba.java
+++ prueba.java
@@ -3,6 +3,9 @@ public class prueba {
         System.out.println("Hola");
         for (int i = 0;i<10;i++){
             System.out.println("Holaaaaaaa");
+            if(i == 8){
+                System.out.println("8");
+            }
         }
     }
 }
